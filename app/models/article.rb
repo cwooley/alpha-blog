4f3d3f68cfa933edf0d9505dd9039b1b9a918c4a
cwooley <cwooley89@gmail.com>
2017-02-28 19:40:57 +0000

    added association betweek user and articles and add debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   # Validation code
   validates :title, presence: true, length: { minimum: 3, maximum: 140}
   validates :description, presence: true, length: { minimum: 10 }
+  validates :user_id, presence: true
   
   
 end
