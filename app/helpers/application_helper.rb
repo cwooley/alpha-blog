c4fbd15a88ca9fe58c34d9f62297490d20918e2a
cwooley <cwooley89@gmail.com>
2017-02-28 23:28:58 +0000

    created show action added gravatar, and articles show partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size: 80})
+    gravatat_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatat_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+  end
 end
